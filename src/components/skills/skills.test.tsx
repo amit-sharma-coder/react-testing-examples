409a1739db01b2876738ca10dfa04379238a809c
Amit Sharma <amitsharma@Amits-MacBook-Air.local>
2023-01-19 10:06:16 +0530

    queryByRole example

--- src/components/skills/skills.test.tsx
+++ src/components/skills/skills.test.tsx
@@ -15,4 +15,16 @@ describe('skills', () => {
         const listItemElements = screen.getAllByRole('listitem');
         expect(listItemElements).toHaveLength(skills.length);
     });
+
+    test('renders login button', () => {
+        render(<Skills skills={skills} />);
+        const loginButton = screen.getByRole('button', {name: 'Login'});
+        expect(loginButton).toBeInTheDocument();
+    });
+
+    test('start learning button not rendered', () => {
+        render(<Skills skills={skills} />);
+        const startLearningButton = screen.queryByRole('button', {name: 'Start Learning'});
+        expect(startLearningButton).not.toBeInTheDocument();
+    });
 });
--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -1,13 +1,20 @@
+import {useState} from 'react';
 import { SkillProps } from "./skills.types"
 
 export const Skills = (props: SkillProps) => {
     const {skills} = props;
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     return (
         <>
             <ul>
                 {skills.map(skill => <li key={skill}>{skill}</li>)}
             </ul>
+            {isLoggedIn ?
+                <button>Start Learning</button> :
+                <button onClick={() => setIsLoggedIn(true)}>
+                    Login</button>
+                }
         </>
     );
 };
