65543ab20e7be6f3ab43b9ecb0185bd4bd8cd7e3
Amit Sharma <amitsharma@Amits-MacBook-Air.local>
2023-01-24 22:07:04 +0530

    skill tests added

--- src/components/skills/skills.test.tsx
+++ src/components/skills/skills.test.tsx
@@ -1,4 +1,4 @@
-import {render, screen} from '@testing-library/react';
+import {render, screen, logRoles} from '@testing-library/react';
 import { Skills } from './skills';
 
 describe('skills', () => {
@@ -29,8 +29,11 @@ describe('skills', () => {
     });
 
     test('start learning button eventually displayed', async () => {
-        render(<Skills skills={skills} />);
+        const view = render(<Skills skills={skills} />);
+        logRoles(view.container);
+        // screen.debug();
         const startLearningButton = await screen.findByRole('button', {name: 'Start Learning'}, { timeout: 2000 });
+        // screen.debug();
         expect(startLearningButton).toBeInTheDocument();
     });
 });
